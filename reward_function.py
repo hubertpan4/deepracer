a0d58ccb1a05fd498aec2b62453977770507c4b9
Hubert Pan <hubert.pan4@gmail.com>
2024-08-11 20:56:37 -0400

    add code to calculate if car will go off track before the next update

--- reward_function.py
+++ reward_function.py
@@ -1,8 +1,8 @@
-# import math
+import math
 # import random
 # import numpy
 # import scipy
-# import shapely
+from shapely.geometry import LineString
 
 update_interval = 1/15
 
@@ -35,8 +35,9 @@ def attempt_race_line(params):
     allWheelsOnTrack:bool = params["all_wheels_on_track"]
     isOffTrack:bool = params["is_offtrack"]
     is_crashed:bool = params["is_crashed"]
-    waypoints = params["waypoints"]
     if(not allWheelsOnTrack or isOffTrack or is_crashed):
+        return -1000
+    elif will_car_run_off_road_before_next_update(params):
         return -1000
     else:
         reward:float = 0.0
@@ -46,18 +47,48 @@ def attempt_race_line(params):
         reward = reward + speed/5.0
 
 
-def will_car_run_off_road_before_next_update(params:map):
+def will_car_run_off_road_before_next_update(params:map) -> bool:
     # raw inputs
     speed:float = params["speed"] # 0.0 to 5.0 m/s
     heading:float = params["heading"] # -180 to 180 degrees relative to x axis of the coordinate system. may need to convert this to relative to the road.
     steering_angle:float = params["steering_angle"] # (right)-30 to 30 (left) relative to the car
     closest_waypoints = params["closest_waypoints"]
     waypoints = params["waypoints"]
-    # generate future route for bounds checking
-    upcoming = spliceWithLoop(waypoints, closest_waypoints[0], closest_waypoints[0] + 5)
-    # calculated vals
+    # generate car's expected future path
     distance_traveled:float = speed * update_interval # m/s * s = m
     corrected_heading = steering_angle + heading
+    slope = math.tan(corrected_heading*(math.pi/180))
+    scale = math.sqrt(math.pow(distance_traveled, 2)/(1 + math.pow(slope, 2)))
+    delta_x = scale
+    delta_y = slope * scale
+    if abs(heading) > 90:
+        delta_x = -1 * delta_x
+    if heading < 0:
+        delta_y = -1 * delta_y
+    car_location = (params['x'], params['y'])
+    predicted_car_location = (params['x'] + delta_x, params['y'] + delta_y)
+    predicted_car_path = LineString([car_location, predicted_car_location])
+    # generate future route for bounds checking
+    track_length:float = params["track_length"] # m
+    num_waypoints = len(params["waypoints"])
+    inter_waypoint_distance = track_length/num_waypoints
+    forward_waypoint_count = math.ceil(distance_traveled/inter_waypoint_distance)
+    forwardRange = forward_waypoint_count * (-1 if params["is_reversed"] else 1)
+    upcomingWayPoints = spliceWithLoop(waypoints, closest_waypoints[0], closest_waypoints[0] + forwardRange)
+    upcomingCenterLine = LineString(upcomingWayPoints)
+    track_width = params["track_width"]
+    upcomingWayPointsLeft = upcomingCenterLine.offset_curve(0.5 * track_width)
+    upcomingWayPointsRight = upcomingCenterLine.offset_curve(-0.5 * track_width)
+    return predicted_car_path.intersects(upcomingWayPointsLeft) or predicted_car_path.intersects(upcomingWayPointsRight)
+    
+
+
+
+def is_car_going_wide_on_turn(params:map):
+    # raw inputs
+    # generate future route for curvature checking
+    # if curvature is high enough reward for going wide.
+    return 0
 
     
 def spliceWithLoop(array: list, start: int, stop: int) -> list:
